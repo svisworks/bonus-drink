d357aa039db4f59a9f2bb94d34a4a55337fcc9a8
svisworks <svisworks@gmail.com>
2015-08-28 13:47:47 +0900

    pg edit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    totalAmount = amount
+    n = [amount, 0]
+    while (n[0] + n[1]) >= 3 do
+      n = (n[0] + n[1]).divmod(3)
+      totalAmount += n[0]
+    end
+    return totalAmount
   end
 end
